61f5af766a14db47524e6ba804e3f0c68d48231a
inh556 <yanbin.jin@gmail.com>
2018-03-19 17:34:53 -0700

    Completed overall view of backcast and have started work on correctly implementing videoListView

--- src/views/app.js
+++ src/views/app.js
@@ -4,11 +4,29 @@ var AppView = Backbone.View.extend({
 
   initialize: function() {
     this.videos = new Videos();
+    this.videos.add(exampleVideoData);
+    this.render();
+    
+    console.log(this.videos);
   },
 
 
   render: function() {
     this.$el.html(this.template());
+
+    //render videoList
+    new VideoListView({
+      collection: this.videos,
+      // $el: $('.list');
+      el: this.$('.list')
+      
+    });
+    new VideoPlayerView ({
+      el: this.$('.player')
+    });
+    new SearchView ({
+      el: this.$('.search')
+    });
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,12 +1,33 @@
 var VideoListView = Backbone.View.extend({
 
-
+  initialize: function() {
+    this.render();
+  },
+  
   render: function() {
+    // debugger;
     this.$el.children().detach();
-    this.$el.html(this.template());
+    // this.$el.html(this.template())
+    // this.$el.html('')
+    this.$el.append(
+      this.collection.map((video) => {
+        return new VideoListEntryView({model: video}).render().el;
+      })
+    );
+    // this.collection.each(function(video) {
+    //   this.$el.children().append(
+    //     new VideoListEntryView( {model: video} ).render()
+    //   );
+    // })
+    // this.$el.children().append(
+    //   this.collection.map(function(data) {
+    //     return new VideoListEntryView( {model: video} ).render();
+    //   })
     return this;
   },
 
   template: templateURL('src/templates/videoList.html')
 
 });
+
+
